989e5dcfc9a9c6e5d06624cf8457c47e2a05ca6f
Senninseyi <seyi.oyebamiji@gmail.com>
2022-02-27 12:05:30 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:userrides/screen/loginScreen.dart';
 import 'package:userrides/screen/mainScreen.dart';
 import 'package:userrides/screen/registerScreen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+DatabaseReference usersRef = FirebaseDatabase.instance.reference().child("users");
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
